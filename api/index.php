dceb347f10f220203196fbfda81d449b63d1240f
Panda <29082270+DAVPanda@users.noreply.github.com>
2024-01-12 15:26:55 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 require_once 'Bot.php';
 
 header('content-type: application/json');
-$token = $_REQUEST['token'] ?? null;
+$token = $_REQUEST['6507183171:AAFY4QUEG-MC9yxDJO6uPOpIMGgdjve18vQ'] ?? null;
 $message = $_REQUEST['message'] ?? '';
 
 $bot = new Bot();
